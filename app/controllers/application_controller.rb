b4fcc530bac159276fccfa2a0e6f32da68d57842
bourbon-f <70570633+bourbon-f@users.noreply.github.com>
2020-10-02 15:46:23 +0900

    ホームページへのアクセス

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def basic_auth
-    authenticate_or_request_with_htpp_basic do |username, password|
+    authenticate_or_request_with_http_basic do |username, password|
       username == 'aaa' && password == '1234'
     end
   end
