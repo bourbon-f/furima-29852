9e7181e9fe204fea695a8533c1bb16e78ced8e1e
bourbon-f <70570633+bourbon-f@users.noreply.github.com>
2020-10-05 15:45:15 +0900

    ユーザー登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
+  # before_action :authenticate_user!
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'aaa' && password == '1234'
+    end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :pass_word, :password_confirmation, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # バリデーション
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+
+  # アソシエーション
+  has_many :items
+  has_many :buys
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
