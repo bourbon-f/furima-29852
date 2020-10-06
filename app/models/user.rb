5c20ce6e5b5d7d1a0af010a9eb9eaec1e39a43fd
bourbon-f <70570633+bourbon-f@users.noreply.github.com>
2020-10-06 16:25:56 +0900

    Revert "Update user.rb"
    This reverts commit 2bdbcdec85359008ff911655c3724cdf5f895364.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,15 @@ class User < ApplicationRecord
   # バリデーション
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
-
-
   with_options presence: true do
-    NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
-    NAME_KANA_REGEX = /\A[ァ-ン]+\z/.freeze
     validates :nickname, length: { maximum: 40 }
     validates :email
     validates :password, length: { minimum: 6 }
     validates :password_confirmation
-    validates :family_name, format: { with:NAME_REGEX}
-    validates :first_name, format: { with:NAME_REGEX}
-    validates :family_name_kana, format: { with: NAME_KANA_REGEX}
-    validates :first_name_kana, format: { with: NAME_KANA_REGEX}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :family_name_kana, format: { with: /\A[ァ-ン]+\z/}
+    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/}
     validates :birthday
   end
 
