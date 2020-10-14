5cdb2e67a9c20ab0d575e9a8a736fc74569c7441
bourbon-f <70570633+bourbon-f@users.noreply.github.com>
2020-10-14 18:49:03 +0900

    編集機能実装の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
-    @item = Item.all.order(id: :DESC)
+    @items = Item.all.order(id: :DESC)
   end
 
   def new
@@ -11,23 +12,20 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if@item.save
-      redirect_to root_path
+    if @item.save
+        redirect_to root_path
     else
       render :new
     end
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
        redirect_to item_path
     else
@@ -44,6 +42,10 @@ class ItemsController < ApplicationController
   
   def item_params
     params.require(:item).permit(:image, :product_name, :description, :genre_id, :status_id, :burden_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
