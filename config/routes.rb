61b61d460e9fdd7b435e950ba537c079c0943efe
bourbon-f <70570633+bourbon-f@users.noreply.github.com>
2020-10-14 14:28:11 +0900

    一覧表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'items#index'
-  resources :items, only: [:index, :new, :create]
+  resources :items do
+   resources :order, only: [:index, :create]
+  end
   resources :users, only: :show
 
 end
